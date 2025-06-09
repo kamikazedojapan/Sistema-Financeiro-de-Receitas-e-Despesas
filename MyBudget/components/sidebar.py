371a00b6f4f1d3eec3599970889079178e3684e3
kamikazedojapan <vi.nimarcio@hormail.com>
2025-06-09 10:05:32 -0300

    title and image profile

--- MyBudget/components/sidebar.py
+++ MyBudget/components/sidebar.py
@@ -17,9 +17,40 @@ import pandas as pd
 
 # ========= Layout ========= #
 layout = dbc.Col([
-                
-            ])
+    html.H1("Home Page", className="text-primary"),
+    html.Hr(),
 
+    # Seção PERFIL----------------
+    dbc.Button(
+        id='botao_avatar',
+        children=[
+            html.Img(
+                src='/assets/img_hom.png',
+                id="avatar_change",
+                alt='Avatar',
+                className='perfil_name',
+                style={
+                    'width': '200px',           # tamanho opcional
+                    'height': 'auto',
+                    'border-radius': '50%',     # se quiser arredondar a imagem
+                    'object-fit': 'cover'
+                }
+            )
+        ],
+        style={
+            'position': 'absolute',
+            'top': '100px',
+            'left': '40px',
+            'background-color': 'transparent',
+            'border': 'none',
+            'padding': '0'
+        }
+    )
+], style={
+    'position': 'relative',
+    'height': '100vh',
+    'padding': '20px'
+})
 
 
 
--- MyBudget/myindex.py
+++ MyBudget/myindex.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
-
+from components import sidebar, dashboards, extratos
 
 
 
@@ -15,15 +15,28 @@ content = html.Div(id="page-content")
 
 
 app.layout = dbc.Container(children=[
-
-
-
-
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'),
+            sidebar.layout
+        ], md=2, style={'background-color': 'white', 'height': '1080px'}),
+        dbc.Col([
+            content
+        ], md=10, style={'background-color': 'white', 'height': '1080px'})
+    ])
 
 ], fluid=True,)
 
 
 
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def render_page(pathname):
+    if pathname == '/' or pathname =='/dashboards':
+        return dashboards.layout
+    if pathname == '/' or pathname =='/extratos':
+        return extratos.layout
+    
+
 
 if __name__ == '__main__':
-    app.run_server(port=8051, debug=True)
+    app.run(port=8051, debug=True)
